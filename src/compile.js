531b65e5ce5f6b5ae01cbfb91b75f4662bf41320
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2023-11-17 11:02:19 +0000

    Compile database query

--- src/compile.js
+++ src/compile.js
@@ -3,6 +3,6 @@ const filter2mapnik = require('./filter2mapnik')
 
 module.exports = function compile (data) {
   const filter = new OverpassFrontend.Filter(data.query)
-  console.log(JSON.stringify(filter.def, null, '  '))
-  console.log(filter2mapnik(filter.def))
+  console.log(filter.sets._)
+  console.log(filter2mapnik(filter.sets._))
 }
--- src/filter2mapnik.js
+++ src/filter2mapnik.js
@@ -1,18 +1,82 @@
-function filter2mapnik (query) {
-  if (query.or) {
-    return query.or.map(f => '(' + filter2mapnik(f) + ')').join(' OR ')
+const dbColumns = ['amenity', 'shop']
+
+function filter2mapnik (query, result = null) {
+  let op
+  let where = []
+
+  if (query.constructor.name === 'FilterOr') {
+    return 'select * from (' + query.parts.map(f => filter2mapnik(f)).join('\nUNION\n') + ') t'
   }
-  else if (query.and) {
-    return query.and.map(f => '(' + filter2mapnik(f) + ')').join(' AND ')
+  else if (query.constructor.name === 'FilterAnd') {
+    return console.log('not supported: FilterAnd')
   }
-  else if (Array.isArray(query)) {
-    return query.map(f => '(' + filter2mapnik(f) + ')').join(' AND ')
-  }
-  else if (query.type) {
-    return 
+  else if (query.constructor.name === 'FilterQuery') {
+    query.filters.map(filter => {
+      switch (filter.op) {
+        case 'has_key':
+          where.push(dbColumns.includes(filter.key) ? colEsc(filter.key) + ' is not null' : 'tags ? ' + strEsc(filter.key))
+          break
+        case 'has_key':
+          where.push(dbColumns.includes(filter.key) ? colEsc(filter.key) + ' is not null' : 'tags ? ' + strEsc(filter.key))
+          break
+        case 'not_exists':
+          where.push(dbColumns.includes(filter.key) ? colEsc(filter.key) + ' is null' : ('tags->' + strEsc(filter.key) + ' is null'))
+          break
+        case '=':
+        case '!=':
+        case '~':
+        case '!~':
+        case '~i':
+        case '!~i':
+          op = filter.op.replace(/i$/, '*')
+          where.push((dbColumns.includes(filter.key) ? colEsc(filter.key) : 'tags->' + strEsc(filter.key)) + ' ' + op + ' ' + strEsc(filter.value))
+          break
+        default:
+          console.log('not supported:', filter)
+      }
+    })
+
+    where = where.length ? where.join(' AND ') : 'true'
+
+    switch (query.type) {
+      case 'node':
+        return "select 'node' as type, osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags from planet_osm_point where " + where
+        break
+      case 'way':
+        return "select * from ((" +
+          "select 'way' as type, osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags, way, 0 as way_area from planet_osm_polygon where osm_id > 0 and " + where +
+          ") union (" +
+          "select 'way' as type, osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags, way, 0 as way_area from planet_osm_line where osm_id > 0 and " + where +
+          ")) t"
+        break
+      case 'relation':
+        return "select * from ((" +
+          "select 'relation' as type, -osm_id as osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags, way, 0 as way_area from planet_osm_polygon where osm_id < 0 and " + where +
+          ") union (" +
+          "select 'relation' as type, -osm_id as osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags, way, 0 as way_area from planet_osm_line where osm_id < 0 and " + where +
+          ")) t"
+        break
+        break
+      case 'nwr':
+        return "select * from ((" +
+          "select 'node' as type, osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags, way, 0 as way_area from planet_osm_point where " + where +
+          ") union (" +
+          "select case when osm_id < 0 then 'relation' else 'way' end as type, abs(osm_id) as osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags, way, 0 as way_area from planet_osm_polygon where " + where +
+          ") union (" +
+          "select case when osm_id < 0 then 'relation' else 'way' end as type, abs(osm_id) as osm_id, hstore(ARRAY[" + dbColumns.map(k => strEsc(k) + ',' + colEsc(k)).join(',') + "]) || tags as tags, way, 0 as way_area from planet_osm_line where " + where +
+          ")) t"
+    }
   }
 
   return ''
 }
 
 module.exports = filter2mapnik
+
+function colEsc (k) {
+  return `"${k}"`
+}
+
+function strEsc (k) {
+  return `'${k}'`
+}
