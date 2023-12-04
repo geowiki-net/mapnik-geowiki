81e9aae18142f0d5503bc5533a169486e0b26c0a
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2023-12-04 10:45:36 +0000

    compileQueries: bugfix, omit ',', if a function does not return fields

--- src/compileQueries.js
+++ src/compileQueries.js
@@ -23,7 +23,7 @@ module.exports = function compileQueries (layers, fields, options) {
     return null
   }).filter(v => v).join(', ')
 
-  let result = 'select ' + selects + ', way from (' + layerQs.join(' union all ') + ') t order by cast(exprs->>\'zIndex\' as float)'
+  let result = 'select ' + selects + (selects !== '' ? ', ' : '') + 'way from (' + layerQs.join(' union all ') + ') t order by cast(exprs->>\'zIndex\' as float)'
   result = result.replace(/</g, '&lt;')
   result = result.replace(/>/g, '&gt;')
   return result
