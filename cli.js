15244175a5c51188d5e64210a3b85f4b8b784b5b
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2023-12-04 08:14:35 +0000

    cli: pass the filename to compile as positional argument

--- cli.js
+++ cli.js
@@ -9,13 +9,17 @@ const parser = new ArgumentParser({
   description: 'Convert a geowiki stylesheet into a Mapnik stylesheet'
 })
 
+parser.add_argument('filename', {
+  help: 'The geowiki stylesheet to compile, e.g. "file.yaml"'
+})
+
 const args = parser.parse_args()
 
 const options = {
   id: 'example'
 }
 
-fs.readFile('example.yaml', (err, body) => {
+fs.readFile(args.filename, (err, body) => {
   if (err) {
     return console.error(err)
   }
