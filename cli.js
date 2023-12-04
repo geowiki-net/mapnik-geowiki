df13c1adde29eec7154bd5b1daad12de82e4b3ed
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2023-12-04 08:12:06 +0000

    cli: use argparse to parse arguments

--- cli.js
+++ cli.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const yaml = require('js-yaml')
+const ArgumentParser = require('argparse').ArgumentParser
 const compile = require('./src/compile')
+
+const parser = new ArgumentParser({
+  add_help: true,
+  description: 'Convert a geowiki stylesheet into a Mapnik stylesheet'
+})
+
+const args = parser.parse_args()
 
 const options = {
   id: 'example'
