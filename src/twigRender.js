04964f871390983efc34edf81fc8192c6728de6f
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2023-12-07 10:38:54 +0000

    twigRender: compile twig template only once (massiv speedup!)

--- src/twigRender.js
+++ src/twigRender.js
@@ -1,5 +1,10 @@
 const twig = require('twig').twig
+const templates = {}
 
 module.exports = function twigRender(data, values) {
-  return twig({data}).render(values)
+  if (!(data in templates)) {
+    templates[data] = twig({data})
+  }
+
+  return templates[data].render(values)
 }
