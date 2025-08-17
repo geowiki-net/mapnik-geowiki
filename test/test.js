5576f22d872660086435984466c42c70afd698df
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-08-17 08:21:16 +0200

    Test: Prepare generated directory

--- test/test.js
+++ test/test.js
@@ -1,10 +1,30 @@
+import fs from 'fs'
+import async from 'async'
 import mapnikGeowiki from '../src/app.js'
 import test from './src/test.js'
 
-//describe('Clear', function () {
-//  it('Remove generated images', function (done) {
-//  })
-//})
+describe('Prepare', function () {
+  it('Check that generated directory exists', function (done) {
+    fs.mkdir('test/generated', (err) => {
+      if (!err || (err && err.code === 'EEXIST')) {
+        return done()
+      }
+
+      done(err)
+    })
+  })
+
+  it('Remove generated images', function (done) {
+    this.timeout(20000)
+    fs.readdir('test/generated', (err, list) => {
+      if (err) { return done(err) }
+
+      if (!list.length) { return done() }
+
+      async.each(list, (item, done) => fs.unlink('test/generated/' + item, done), done)
+    })
+  })
+})
 
 describe('Render from parameters', function () {
   it('Render buildings', function (done) {
