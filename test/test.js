2118167fe43d624f3b7f18bd000e7c8139dc595c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-08-20 10:07:56 +0200

    Tests: test CircleMarker

--- test/test.js
+++ test/test.js
@@ -73,4 +73,19 @@ describe('Render from parameters', function () {
       test('2.svg', done)
     })
   })
+
+  it('Render with CircleMarker', function (done) {
+    this.timeout(20000)
+    mapnikGeowiki({
+      size: '100x100',
+      bbox: '48.19878,16.33585,48.19988,16.33743',
+      style: 'test/CircleMarker.yaml',
+      source: 'test/data.osm.bz2',
+      output: 'test/generated/3.svg',
+    }, function (err, result) {
+      if (err) { return done(err) }
+      test('3.svg', done)
+    })
+  })
+
 })
