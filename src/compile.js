d9df2c7234245ce88e5f40ced458bf3784db31a5
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-08-20 10:13:47 +0200

    Get rid of code for PostgreSQL PLv8 functions

--- src/compile.js
+++ src/compile.js
@@ -1,8 +1,7 @@
 const styles2mapnik = require('./styles2mapnik')
 const getStyleFieldValues = require('./getStyleFieldValues')
 const fs = require('fs')
-const compileSelects = require('./compileSelects')
-const createPLV8Functions = require('./createPLV8Functions')
+const path = require('path')
 const getZoomLevels = require('./getZoomLevels')
 const zoomToScale = require('./zoomToScale')
 const twigRender = require('./twigRender')
@@ -23,22 +22,8 @@ module.exports = function compile (data, options) {
     const maxZoom = zoomLevels[i + 1]
     const minScale = zoomToScale(maxZoom ?? 30)
 
-    const tables = layers.map(l => {
-      if (!l.zoomLevelActive(zoom)) {
-        return null
-      }
-
-      return l.compileLayerTable()
-    }).filter(s => s).join(' union all ')
-
-    let selects = compileSelects(tables, styleFieldValues, options)
-
-    selects = selects.replace(/</g, '&lt;')
-    selects = selects.replace(/>/g, '&gt;')
-
     let layer = templateLayer.replace(/%layerid%/g, 'ID' + i)
     layer = layer.replace(/%styleid%/g, 'ID')
-    layer = layer.split('%query%').join(selects)
     layer = layer.replace('%minScale%', minScale)
     layer = layer.replace('%maxScale%', maxScale)
 
@@ -51,19 +36,6 @@ module.exports = function compile (data, options) {
   stylesheet = stylesheet.replace(/%id%/g, 'ID')
   stylesheet = stylesheet.replace('%rules%', rules)
   stylesheet = stylesheet.split('%layers%').join(mapnikLayers)
-
-  const sqlFuncs = layers.map(l => l.compileFunction(styleFieldValues))
-
-  createPLV8Functions(sqlFuncs, options, (err, file) => {
-    if (err) { return console.error(err) }
-    const filename = options.id + '.sql'
-
-    fs.writeFile(filename, file, (err) => {
-      if (err) { return console.error(err) }
-
-      console.log('create ' + filename)
-    })
-  })
 
   return stylesheet
 }
