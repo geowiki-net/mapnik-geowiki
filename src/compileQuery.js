cddbfdaa607fc181679ff1783ce306e98d22df2c
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2026-01-22 11:54:35 +0100

    The query of a layer may use TwigJS templates

--- src/compileQuery.js
+++ src/compileQuery.js
@@ -1,7 +1,18 @@
 const OverpassFrontend = require('overpass-frontend')
 const filter2mapnik = require('./filter2mapnik')
+const compileTemplate = require('geowiki-layer/src/compileTemplate.js')
+const twig = require('twig')
 
 module.exports = function (query, options) {
+  const twigData = {
+    parameters: options.parameters
+  }
+
+  const template = compileTemplate(query, twig)
+  if (typeof template === 'function') {
+    query = template(twigData)
+  }
+
   const filter = new OverpassFrontend.Filter(query)
   return filter2mapnik(filter.sets._)
 }
