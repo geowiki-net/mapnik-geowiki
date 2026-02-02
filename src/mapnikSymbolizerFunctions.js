68a5331d8f3ce27c6a2b454da8aa2b36634cdfa6
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-02-02 22:47:38 +0100

    field 'textRepeat', 'textRepeatSpacing': calculate combined value

--- src/mapnikSymbolizerFunctions.js
+++ src/mapnikSymbolizerFunctions.js
@@ -3,4 +3,14 @@
    - fun: a function which will calculate the final value
 */
 module.exports = {
+  _spacing: {
+    depend: [ 'textRepeat', 'textRepeatSpacing' ],
+    fun: properties => {
+      if (properties.textRepeat) {
+        return properties.textRepeatSpacing === 0 ? 0.0001 : properties.textRepeatSpacing
+      } else {
+        return 0
+      }
+    }
+  }
 }
