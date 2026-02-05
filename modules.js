6b4cfc3398c464b8dc3c8e600322e60ee9934747
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-02-05 07:53:06 +0100

    Include various twig functions

--- modules.js
+++ modules.js
@@ -1,7 +1,13 @@
 module.exports = [
+  // Various twig functions
+  require('@geowiki-net/geowiki-twig-functions'),
+
   // Geometric functions using the Turf.js library
   require('geowiki-module-turf'),
 
   // Merge line strings
   require('geowiki-module-merge-line-strings'),
+
+  // Enable support for color functions
+  require('geowiki-module-color'),
 ]
