440812f8a842a81dfb694a9f9dcaa36a21b49c76
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-03-22 07:40:32 +0100

    Include support for UMap

--- modules.js
+++ modules.js
@@ -13,4 +13,7 @@ module.exports = [
 
   // Enable support for color functions
   require('geowiki-module-color'),
+
+  // Suport for UMap files (parsing & style)
+  require('@geowiki-net/geowiki-umap'),
 ]
