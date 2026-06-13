6b02cbb920534b4cda424108a89f6577efc86956
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-06-13 08:14:56 +0200

    render2GeoJSON: fix parsing array-length

--- src/render2GeoJSON.js
+++ src/render2GeoJSON.js
@@ -44,7 +44,7 @@ module.exports = function render2GeoJSON (list, options) {
               properties[key] = parseLength(properties[key], metersPerPixel)
               break
             case 'array-length':
-              properties[key] = properties[key].split(/,/g).map(v => parseLength(v, metersPerPixel)).join(',')
+              properties[key] = ('' + properties[key]).split(/,/g).map(v => parseLength(v, metersPerPixel)).join(',')
               break
           }
 
